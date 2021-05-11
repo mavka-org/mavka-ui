3fd981ae3c579a5afb7de455cdd61d84ba1dc82c
Artem <arlaptiev@gmail.com>
2021-05-11 23:10:22 +0300

    Removed carousel

--- src/stories/navigation/TestTopNavBar.stories.js
+++ src/stories/navigation/TestTopNavBar.stories.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { TestTopNavBar } from '../../components/navigation/TestTopNavBar';
+import TestRadioInputSection from '../../components/userInput/TestRadioInputSection';
+import TestRadioButton from '../../components/elements/TestRadioButton';
 import { Typography } from '@material-ui/core';
 
 export default {
@@ -8,11 +10,41 @@ export default {
   component: TestTopNavBar
 };
 
-export const Default = () => <TestTopNavBar 
-  exitOnClick={() => {alert('Exiting!')}} 
-  collapsedTitle='Завдання 1'>
-    <Typography variant='h3'>Завдання 1</Typography>
-    These are children of the TestTopNavBar
-  </TestTopNavBar>
+export const Default = () => { 
+  const [collapsed, setCollapsed] = useState(false);
 
-export const NoCollapse = () => <TestTopNavBar exitOnClick={() => {alert('Exiting!')}} noCollapse>These are children of the TestTopNavBar. This TestTopNavBar doesn't collapse</TestTopNavBar>
+  useEffect(() => {
+    if (collapsed) {
+      return;
+    }
+  
+    const handleScroll = () => {
+      setCollapsed(true);
+    }
+  
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [collapsed]);
+
+  return(
+    <TestTopNavBar 
+      exitOnClick={() => {alert('Exiting!')}}
+      collapsedTitle='Завдання 1'
+      setCollapsed={setCollapsed} 
+      collapsed={collapsed}
+    >
+      <Typography variant='h3'>Завдання 1</Typography>
+      <Typography variant='body2' fontWeight='fontWeightMedium'>2019 основна секція</Typography>
+      <TestRadioInputSection
+        testRadioButtonsList={[
+          <TestRadioButton label="1"/>, <TestRadioButton label="2"/>, <TestRadioButton label="3"/>, <TestRadioButton label="4"/>, <TestRadioButton label="5"/>, 
+          <TestRadioButton label="6"/>, <TestRadioButton label="7"/>, <TestRadioButton label="8"/>, <TestRadioButton label="9"/>, <TestRadioButton label="10"/>, 
+          <TestRadioButton label="11"/>, <TestRadioButton label="12"/>, <TestRadioButton label="13"/>, <TestRadioButton label="14"/>, <TestRadioButton label="15"/>,
+          <TestRadioButton label="16"/>, <TestRadioButton label="17"/>, <TestRadioButton label="18"/>, <TestRadioButton label="19"/>, <TestRadioButton label="20"/>,
+          <TestRadioButton label="21"/>, <TestRadioButton label="22"/>, <TestRadioButton label="23"/>, <TestRadioButton label="24"/>, <TestRadioButton label="25"/>,
+          <TestRadioButton label="26"/>, <TestRadioButton label="27"/>, <TestRadioButton label="28"/>, <TestRadioButton label="29"/>, <TestRadioButton label="30"/>,
+        ]}
+      />
+    </TestTopNavBar>
+  )
+}
