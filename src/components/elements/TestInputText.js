1fb5e06bb9ef3e003b4cab0728f0c157798b65f0
Makarov Danil <ninjaheister@gmail.com>
2021-05-15 12:36:10 +0200

    buttonWithIcon

--- src/components/elements/TestInputText.js
+++ src/components/elements/TestInputText.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import { getColorByCorrectness, getBackgroundColorByCorrectness } from '../../helpers';
+import { getColorByCorrectness, getBackgroundColorByCorrectness, getInputColorByCorrectness } from '../../helpers';
 import '../../fonts/style.css'
 
 const useStyles = props => makeStyles(theme => ({
   root: {
     width: '90px',
     height: '25px',
-    border: `3px solid ${getColorByCorrectness(props.correctness, props.userInput, theme)}`,
+    border: `3px solid ${getInputColorByCorrectness(props.correctness, props.userInput, theme)}`,
     borderRadius:'16px',
     outline:'none',
     paddingLeft:'14px',
     fontFamily:'SF UI Display',
     fontWeight:'600',
     fontSize:'14px',
-    color:`${getColorByCorrectness(props.correctness, props.userInput, theme)}`,
+    color:`${getInputColorByCorrectness(props.correctness, props.userInput, theme)}`,
     backgroundColor:`${getBackgroundColorByCorrectness(props.correctness, theme)}`
   },
 }));
