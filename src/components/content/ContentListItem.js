f36fde93ab9d786016af67f817e284271fa4d1d5
Artem <arlaptiev@gmail.com>
2021-05-17 18:58:37 +0300

    edit: contentListItem

--- src/components/content/ContentListItem.js
+++ src/components/content/ContentListItem.js
@@ -5,15 +5,15 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 const useStyles = props => makeStyles( theme => ({
   root: {
-   
+    flexWrap: 'nowrap'
   },
 }));
 
 export function ContentListItem({ children, ...props }){
   const classes = useStyles(props)();
   return (
-    <Grid container direction='row' alignItems="center" className={classes.root}>
-      <Grid item style={{marginRight:'15px'}}><Typography>{props.label}:</Typography></Grid>
+    <Grid container direction='row' alignItems="baseline" className={classes.root}>
+      <Grid item style={{marginRight:'15px'}}><Typography><strong>{props.label}:</strong></Typography></Grid>
       <Grid item><Typography>{children}</Typography></Grid>
     </Grid>
   );
