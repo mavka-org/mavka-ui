6259056e8326b5330e053f24f57e27b9c3034455
ToniaZakorchemna <antoninazakorchemna@gmail.com>
2021-05-19 13:08:19 +0300

    testInputText fixed default component

--- src/components/elements/TestInputText.js
+++ src/components/elements/TestInputText.js
@@ -24,7 +24,12 @@ export function TestInputText({ children, ...props }) {
   const classes = useStyles(props)();
 
   return (
-    <input className={classes.root} placeholder={props.userInput || null} value={props.userInput} readOnly={props.readOnly} onChange={(event) => props.onChange(event)} />
+    <input 
+    className={classes.root} 
+    placeholder={props.userInput || null} 
+    value={props.userInput ? props.userInput : "Відповідь"} 
+    readOnly={props.readOnly} 
+    onChange={(event) => props.onChange(event)} />
   );
 }
 
