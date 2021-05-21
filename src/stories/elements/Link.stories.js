ab4866af9819a1d4e99ff8433476a4e60c4cd8be
Artem <arlaptiev@gmail.com>
2021-05-21 16:13:52 +0300

    feat: link

---
+++ src/stories/elements/Link.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Link from '../../components/elements/Link';
+import { Router, Route } from 'react-router-dom';
+
+export default {
+  title: 'Elements/Link',
+  component: Link
+};
+
+
+export const Default = () => <Link>This is a Default Link used to pad all the pages.</Link>
